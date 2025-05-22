f97e52a14cd8a7f5ec3c6881ceb331bd69f59e6a
derlocke-ng <120924385+derlocke-ng@users.noreply.github.com>
2025-05-22 09:57:21 +0200

    added ksslgen + readme

---
+++ ksslgen/Readme.md
@@ -0,0 +1,10 @@
+# ksshgen
+*bash script for creating self-signed ssl certs for self-hosting.*
+
+## How to use:
+create new CA:
+Run `./ksslgen.sh new_ca`.
+create new self-signed wildcard ssl cert:
+Run `./ksslgen.sh new_cert example.com`.
+
+Edit script for changing TLDs and other settings. Pre-configured to work with the kiwi-network setup.
---
+++ ksslgen/ksslgen.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+### Usage
+## create new CA
+# ./ksslgen.sh new_ca
+## create new self-signed wildcard ssl cert
+# ./ksslgen.sh new_cert example.com
+
+# Function to create a new kiwiCA
+new_ca() {
+  openssl genrsa -des3 -out kiwiCA.key 4096
+  openssl req -x509 -new -nodes -key kiwiCA.key -sha256 -days 3650 -out kiwiCA.pem -subj "/C=NZ/ST=kiwi/L=kiwi/O=kiwi/OU=kiwi/CN=kiwiCA"
+}
+
+# Function to use an kiwiCA and create new certificates
+new_cert() {
+  local DOMAIN="$2"
+  openssl genrsa -out "${DOMAIN}.key" 4096
+  openssl req -new -key "${DOMAIN}.key" -out "${DOMAIN}.csr" -subj "/C=NZ/ST=kiwi/L=kiwi/O=kiwi/OU=kiwi/CN=${DOMAIN}"
+  cat > openssl.cnf << EOL
+authorityKeyIdentifier=keyid,issuer
+basicConstraints=CA:FALSE
+keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
+subjectAltName = @alt_names
+
+[alt_names]
+DNS.1 = ${DOMAIN}
+DNS.2 = *.${DOMAIN}
+EOL
+  openssl x509 -req -in "${DOMAIN}.csr" -CA kiwiCA.pem -CAkey kiwiCA.key -CAcreateserial -out "${DOMAIN}.pem" -days 3650 -sha256 -extfile openssl.cnf
+  rm -f openssl.cnf
+}
+
+case $1 in
+  new_ca|new_cert) "$1" "$@" ;;
+esac
